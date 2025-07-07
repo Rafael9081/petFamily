09bc7fadf728df8f126957a729ac8f04d158968b
Rafael Sousa <rafael9081@gmail.com>
2025-07-07 05:37:48 -0300

    mudancas no application.properties

--- backend/.mvn/mvnw.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set MVNW_CMD=.mvn\wrapper\maven-wrapper.jar
-IF NOT EXIST "%MVNW_CMD%" (
-  echo Error: %MVNW_CMD% not found. Please run 'mvn -N io.takari:maven:wrapper'
-  exit /B 1
-)
-java -jar %MVNW_CMD% %*
